d9674af38667ce0475ef77630fbfbb6a14c47141
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-04 13:49:53 -0700

    Moved the router variables to index.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 // Variables that are needed for application
-
 const express = require('express');
 const path  = require('path');
 const notesData = require('./db/db.json');
 const fs = require('fs');
 const util = require('util');
+const api = require('./routes/index.js');
 
-
-// Route variables created
-const notesRouter = require('./routes/notesRouter.js');
-const indexRouter = require('./routes/indexRouter');
-const dbRouter = require('./routes/dbRouter.js');
 
 const port = 3001;
 
@@ -19,8 +14,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
+app.use('/api/notes', api);
 
 
 // GET Route for index.html
@@ -33,8 +27,6 @@ app.get('/notes', (req,res)=>
     res.sendFile(path.json(__dirname, '/public/notes.html'))
 );
 
-// Loads the dbRouter module to the server
-app.use('/api/notes', dbRouter);
 
 app.listen(port, () =>
     console.log(`App is listening at http://localhost:${port} 🚀`)
