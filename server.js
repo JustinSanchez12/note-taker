d245f2027bdc6f77b09ee50a7b5ed8d2313f0438
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-04 14:48:40 -0700

    changed the path.json to path.join for routes to work

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
-// Variables that are needed for application
 const express = require('express');
-const path  = require('path');
+const path = require('path');
 const api = require('./routes/index.js');
 
-
 const port = process.env.PORT || 3001;
-
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// API routes
 app.use('/api/notes', api);
 
-
-// GET Route for index.html
-app.get('*', (req,res) => 
-    res.sendFile(path.json(__dirname, '/public/index.html'))
+// GET route for index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 );
 
 // GET Route for notes.html
-app.get('/notes', (req,res)=>
-    res.sendFile(path.json(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 );
 
-
 app.listen(port, () =>
-    console.log(`App is listening at http://localhost:${port} 🚀`)
+  console.log(`App is listening at http://localhost:${port} 🚀`)
 );
