efc3fba5a58363b74312c7f4797629021904f7ff
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-04 14:49:02 -0700

    changed the modular export to notesRouter

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -3,17 +3,10 @@ const notesRouter = express.Router();
 const {readFromFile, readAndAppend} = require('../helpers/fsUtil.js');
 
 
-//TODO: Routes for GET api/notes should read db.json
 notesRouter.get('/api/notes', (req,res) =>{
     console.info(`${req.method} request has been received`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
-
-
-//TODO: Routes for POST api/notes
-/* TODO: hould receive a new note to save on the request body, add it to the db.json file, 
- and then return the new note to the client. You'll need to find a way to give each note a unique
-  id when it's saved (look into npm packages that could do this for you).*/
 
 notesRouter.post('/api/notes', (req,res) =>{
     const {title, text} = req.body;
@@ -24,7 +17,7 @@ notesRouter.post('/api/notes', (req,res) =>{
             text
         };
 
-        readAndAppend(newNotes, '../db/db.json');
+        readAndAppend(newNotes, './db/db.json');
 
         const response = {
             status: 'success',
