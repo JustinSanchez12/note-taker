cb775f734710df4e3a91fa1336ac1e1d9ca93a25
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-05 00:09:33 -0700

    added some comments

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const notesRouter = express.Router();
 const {readFromFile, readAndAppend} = require('../helpers/fsUtil.js');
 
-
+//GET the route for the db.json file, this is where the notes will be read
 notesRouter.get('/api/notes', (req,res) =>{
     console.info(`${req.method} request has been received`);
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
+//POST routes for db.json file, this is where the notes will be posted and if it done correctly it will display a success message otherwise it will send an error message
 notesRouter.post('/api/notes', (req,res) =>{
     const {title, text} = req.body;
 
