629972bad8802b50cc58717d9638b8e0bdb8ef7f
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-05 20:56:58 -0700

    Cleaned up the routes to avoid duplicates

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const notesRouter = express.Router();
 const {readFromFile, readAndAppend} = require('../helpers/fsUtil.js');
+const { v4: uuidv4 } = require('uuid');
 
 //GET the route for the db.json file, this is where the notes will be read
-notesRouter.get('/api/notes', (req,res) =>{
+notesRouter.get('/', (req,res) =>{
     console.info(`${req.method} request has been received`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 //POST routes for db.json file, this is where the notes will be posted and if it done correctly it will display a success message otherwise it will send an error message
-notesRouter.post('/api/notes', (req,res) =>{
+notesRouter.post('/', (req,res) =>{
     const {title, text} = req.body;
 
     if(title && text) {
         const newNotes = {
             title,
-            text
+            text,
+            id: uuidv4()
         };
 
-        readAndAppend(newNotes, '../db/db.json');
+        readAndAppend(newNotes, './db/db.json');
 
         const response = {
             status: 'success',
